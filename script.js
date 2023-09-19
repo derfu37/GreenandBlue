85fb4952d246203452914130ccf038744307e15a
derfu37 <145419934+derfu37@users.noreply.github.com>
2023-09-18 22:25:59 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,298 @@
+
+
+let hideelfaro_btn=document.getElementById("hideelfaro_btn");
+
+let hideelfaro=document.getElementById("hideelfaro");
+
+hideelfaro_btn.addEventListener("click", toggleText);
+
+function toggleText() {
+    hideelfaro.classList.toggle("show");
+}
+
+let hidetela_btn=document.getElementById("hidetela_btn");
+
+let hidetela=document.getElementById("hidetela");
+
+hidetela_btn.addEventListener("click", toggleTela);
+
+function toggleTela() {
+    hidetela.classList.toggle("show");
+}
+
+let hideomoa_btn=document.getElementById("hideomoa_btn");
+
+let hideomoa=document.getElementById("hideomoa");
+
+hideomoa_btn.addEventListener("click", toggleOmoa);
+
+function toggleOmoa() {
+    hideomoa.classList.toggle("show");
+}
+
+
+let hidevenado_btn=document.getElementById("hidevenado_btn");
+
+let hidevenado=document.getElementById("hidevenado");
+
+hidevenado_btn.addEventListener("click", toggleVenado);
+
+function toggleVenado() {
+    hidevenado.classList.toggle("show");
+}
+
+
+let hideceiba_btn=document.getElementById("hideceiba_btn");
+
+let hideceiba=document.getElementById("hideceiba");
+
+hideceiba_btn.addEventListener("click", toggleCeiba);
+
+function toggleCeiba() {
+    hideceiba.classList.toggle("show");
+}
+
+
+let hidepc_btn=document.getElementById("hidepc_btn");
+
+let hidepc=document.getElementById("hidepc");
+
+hidepc_btn.addEventListener("click", togglePc);
+
+function togglePc() {
+    hidepc.classList.toggle("show");
+}
+
+
+
+let hidepd_btn=document.getElementById("hidepd_btn");
+
+let hidepd=document.getElementById("hidepd");
+
+hidepd_btn.addEventListener("click", togglePd);
+
+function togglePd() {
+    hidepd.classList.toggle("show");
+}
+
+
+let hidepp_btn=document.getElementById("hidepp_btn");
+
+let hidepp=document.getElementById("hidepp");
+
+hidepp_btn.addEventListener("click", togglePp);
+
+function togglePp() {
+    hidepp.classList.toggle("show");
+}
+
+
+let hidecienaguita_btn=document.getElementById("hidecienaguita_btn");
+
+let hidecienaguita=document.getElementById("hidecienaguita");
+
+hidecienaguita_btn.addEventListener("click", toggleCienaguita);
+
+function toggleCienaguita() {
+    hidecienaguita.classList.toggle("show");
+}
+
+
+let hidemr_btn=document.getElementById("hidemr_btn");
+
+let hidemr=document.getElementById("hidemr");
+
+hidemr_btn.addEventListener("click", toggleMr);
+
+function toggleMr() {
+    hidemr.classList.toggle("show");
+}
+
+let hide1_btn=document.getElementById("hide1_btn");
+
+let hide1=document.getElementById("hide1");
+
+hide1_btn.addEventListener("click", toggle1);
+
+function toggle1() {
+    hide1.classList.toggle("show");
+}
+
+
+let hide2_btn=document.getElementById("hide2_btn");
+
+let hide2=document.getElementById("hide2");
+
+hide2_btn.addEventListener("click", toggle2);
+
+function toggle2() {
+    hide2.classList.toggle("show");
+}
+
+
+let hide3_btn=document.getElementById("hide3_btn");
+
+let hide3=document.getElementById("hide3");
+
+hide3_btn.addEventListener("click", toggle3);
+
+function toggle3() {
+    hide3.classList.toggle("show");
+}
+
+
+let hide4_btn=document.getElementById("hide4_btn");
+
+let hide4=document.getElementById("hide4");
+
+hide4_btn.addEventListener("click", toggle4);
+
+function toggle4() {
+    hide4.classList.toggle("show");
+}
+
+
+let hide5_btn=document.getElementById("hide5_btn");
+
+let hide5=document.getElementById("hide5");
+
+hide5_btn.addEventListener("click", toggle5);
+
+function toggle5() {
+    hide5.classList.toggle("show");
+}
+
+
+let hide6_btn=document.getElementById("hide6_btn");
+
+let hide6=document.getElementById("hide6");
+
+hide6_btn.addEventListener("click", toggle6);
+
+function toggle6() {
+    hide6.classList.toggle("show");
+}
+
+
+
+
+let hide8_btn=document.getElementById("hide8_btn");
+
+let hide8=document.getElementById("hide8");
+
+hide8_btn.addEventListener("click", toggle8);
+
+function toggle8() {
+    hide8.classList.toggle("show");
+}
+
+
+let hide9_btn=document.getElementById("hide9_btn");
+
+let hide9=document.getElementById("hide9");
+
+hide9_btn.addEventListener("click", toggle9);
+
+function toggle9() {
+    hide9.classList.toggle("show");
+}
+
+
+let hide10_btn=document.getElementById("hide10_btn");
+
+let hide10=document.getElementById("hide10");
+
+hide10_btn.addEventListener("click", toggle10);
+
+function toggle10() {
+    hide10.classList.toggle("show");
+}
+
+
+let hide11_btn=document.getElementById("hide11_btn");
+
+let hide11=document.getElementById("hide11");
+
+hide11_btn.addEventListener("click", toggle11);
+
+function toggle11() {
+    hide11.classList.toggle("show");
+}
+
+
+let hide12_btn=document.getElementById("hide12_btn");
+
+let hide12=document.getElementById("hide12");
+
+hide12_btn.addEventListener("click", toggle12);
+
+function toggle12() {
+    hide12.classList.toggle("show");
+}
+
+
+let hide13_btn=document.getElementById("hide13_btn");
+
+let hide13=document.getElementById("hide13");
+
+hide13_btn.addEventListener("click", toggle13);
+
+function toggle13() {
+    hide13.classList.toggle("show");
+}
+
+
+let hide14_btn=document.getElementById("hide14_btn");
+
+let hide14=document.getElementById("hide14");
+
+hide14_btn.addEventListener("click", toggle14);
+
+function toggle14() {
+    hide14.classList.toggle("show");
+}
+
+
+let hide16_btn=document.getElementById("hide16_btn");
+
+let hide16=document.getElementById("hide16");
+
+hide16_btn.addEventListener("click", toggle16);
+
+function toggle16() {
+    hide16.classList.toggle("show");
+}
+
+
+let hide17_btn=document.getElementById("hide17_btn");
+
+let hide17=document.getElementById("hide17");
+
+hide17_btn.addEventListener("click", toggle17);
+
+function toggle17() {
+    hide17.classList.toggle("show");
+}
+
+
+let hide18_btn=document.getElementById("hide18_btn");
+
+let hide18=document.getElementById("hide18");
+
+hide18_btn.addEventListener("click", toggle18);
+
+function toggle18() {
+    hide18.classList.toggle("show");
+}
+
+
+let hide19_btn=document.getElementById("hide19_btn");
+
+let hide19=document.getElementById("hide19");
+
+hide19_btn.addEventListener("click", toggle19);
+
+function toggle19() {
+    hide19.classList.toggle("show");
+}
+
